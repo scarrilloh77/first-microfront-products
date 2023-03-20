e77e30e861147ebe3bea1f32aa46c072b26fd81b
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-03-20 13:47:48 -0500

    consuming remote modules

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
       name: 'products',
       filename: 'remoteEntry.js', // It lists different modules that can be loaded from the Products project
       exposes: {
-        './ProductsIndex': './src/index.js',
+        './ProductsIndex': './src/bootstrap',
       },
       shared: ['faker'],
     }),
